0b85ace5df397b433f2394749b72faf3f7313706
Raiford <raiford@outlook.com>
2014-10-28 02:42:13 -0700

    Added inial Bullets / ActionDataObjects

---
+++ Source/PowerBlocksSolution/PowerBlocks/Bullets/ActionData.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PowerBlocks.Bullets
+{
+    class ActionDataEnvelope
+    {
+        Guid Uid;
+        ScheduleInfo schedule;
+        int Prioty; // 1-100
+
+
+        ActionData ActionData;
+
+
+
+
+    }
+
+    class ActionDataBuilder
+    {
+        CreateFromObject<T>(T obj)
+
+    
+
+
+    }
+
+    class ActionDataProcessorExecuter
+    {
+        // ProcessQueue
+    }
+
+    class ActionData
+    {
+
+        // what is dooes
+        // - Execute w/ argument payload
+        // - structuredData
+        // - create object
+        // - 
+
+        string Type
+        string AssemblyInfo
+        string InterfaceName
+        string InterfaceMethod
+        ActionDataPayload  Payload; // packets of arguments passed to object   dictionary
+
+
+
+        object ObjectData
+        object 
+
+
+    }
+}
+
+
+
+/
---
+++ Source/PowerBlocksSolution/PowerBlocks/Bullets/Readme.txt
@@ -0,0 +1,14 @@
+﻿
+
+
+
+RPC 
+Execute objects over the internet without web services
+Fire Bullets of Actions across the network
+
+only requirement is .NET and the object data type installed
+
+serializing actiondatas
+
+scheduling action datas
+
