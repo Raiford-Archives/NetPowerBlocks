9b8de5cda01fd9a779b3013f14fc1169cb48d030
Raiford <raiford@outlook.com>
2014-10-28 20:39:12 -0700

    Added Exmaple Web Project

---
+++ Source/PowerBlocksSolution/Example.Web/Example.Web/Areas/Configuration/Controllers/ConfigurationController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PowerBlocks.Configuration;
+
+namespace Example.Web.Areas.Configuration.Controllers
+{
+    public class ConfigurationController : Controller
+    {
+        //
+        // GET: /Configuration/Configuration/
+
+        public ActionResult Index()
+        {
+            
+            // Pull Basic Settings from Source
+            string environmentMode = SettingsWeb.EnvironmentMode.GetString();
+            
+            string serviceUrl1 = SettingsWeb.ServiceUrl1.GetString();
+            string serviceUrl2 = SettingsWeb.ServiceUrl2.GetString();
+            string serviceUrl3 = SettingsWeb.ServiceUrl3.GetString();
+
+            // Now pull an int
+            int timeoutDb = SettingsWeb.DbTimeout.GetInt();
+
+            
+            // Now pull an optional setting and automatically convert it do a Bool
+            bool enableTrace = SettingsWeb.FullTraceEnabled.GetBoolean();
+
+
+            // You can also pull settings from the Common settings in a dependent project
+            string email = Settings.EmailAddressAdmin.GetString();
+            string urlImages = Settings.UrlImages.GetString();
+
+
+            return View();
+        }
+
+    }
+}
---
+++ Source/PowerBlocksSolution/Example.Web/Example.Web/SettingsWeb.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PowerBlocks.Configuration;
+
+namespace Example.Web
+{
+    // do not require comments on the settings, they are self documenting
+#pragma warning disable 1591
+
+    /// <summary>
+    /// Settings for the Web project
+    /// </summary>
+    public static class SettingsWeb
+    {
+        private const bool Optional = true;
+
+
+        // General settings
+        public static readonly ISetting EnvironmentMode = new AppConfigSetting("EnvironmentMode", Optional, "DEV");
+        public static readonly ISetting ServiceUrl1 = new AppConfigSetting("ServiceUrl1");
+        public static readonly ISetting ServiceUrl2 = new AppConfigSetting("ServiceUrl2");
+        public static readonly ISetting ServiceUrl3 = new AppConfigSetting("ServiceUrl3");
+
+
+        // Database stuff
+        public static readonly ISetting DbTimeout = new AppConfigSetting("DBTimeout");
+        public static readonly ISetting FullTraceEnabled = new AppConfigSetting("FullTraceEnabled", Optional, "0");
+
+        // Static Read only settings
+        public static readonly ISetting MyStaticSetting = new ReadOnlySetting("MyStaticSetting");
+
+
+
+    }
+#pragma warning restore 1591
+
+}
